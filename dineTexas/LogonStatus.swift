6ce96d466dc1d907b761a9ec8713625035be5a73
Davidson <mmd2468@cpe-172-100-3-253.twcny.res.rr.com>
2017-03-16 06:20:54 -0500

    Created LogonStatus and LogoViewController. Also set segue depending on if use is logged in or not

---
+++ dineTexas/LogonStatus.swift
@@ -0,0 +1,13 @@
+//
+//  LogonStatus.swift
+//  dineTexas
+//
+//  Created by Davidson, Matthew on 3/16/17.
+//  Copyright © 2017 Hyun Joong Kim. All rights reserved.
+//
+
+import UIKit
+
+class LogonStatus: NSObject {
+
+}
