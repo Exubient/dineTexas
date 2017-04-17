0f9eada8a490641ea66d3df2160cf8b27773576c
Davidson <mmd2468@cns-it-mdpro2.local>
2017-04-16 20:01:23 -0500

    Settings are now set in coreData.. will keep it's state after the app closes

--- dineTexas/SettingVC.swift
+++ dineTexas/SettingVC.swift
@@ -7,17 +7,44 @@
 //
 
 import UIKit
-
+import CoreData
 class SettingViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var greenInput: UITextField!
     @IBOutlet weak var orangeInput: UITextField!
+    @IBOutlet weak var notifications: UISwitch!
+    @IBOutlet weak var rememberLogin: UISwitch!
+    
     var alertController:UIAlertController? = nil
-   
+    var settings = [NSManagedObject]()
     override func viewDidLoad() {
         super.viewDidLoad()
         greenInput.delegate = self
         orangeInput.delegate = self
-        // Do any additional setup after loading the view.
+        loadData()
+        print (settings.count)
+        if ( settings.count >= 1 ) {
+            let setting = settings[ settings.count - 1 ]
+            let gSetting = setting.value(forKey: "greenSetting") as? Int
+            let oSetting = setting.value(forKey: "orangeSetting") as? Int
+            let gSettingString = String(describing: gSetting)
+            let oSettingString = String(describing: oSetting)
+            print(gSettingString)
+            print(oSettingString)
+            if (gSettingString != "" && oSettingString != ""){
+                greenInput.text = String(gSetting!)
+                orangeInput.text = String(oSetting!)
+            }
+            
+//            
+//            greenInput.text = setting.value(forKey: "greenSetting") as? String
+//            orangeInput.text = setting.value(forKey: "orangeSetting") as? String
+            notifications.setOn((setting.value(forKey: "notifications") as? Bool)!, animated: false)
+            rememberLogin.setOn((setting.value(forKey: "rememberLogin") as? Bool)!, animated: false)
+        } else {
+            greenInput.text = "10"
+            orangeInput.text = "20"
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +53,21 @@ class SettingViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func updateSettings(_ sender: Any) {
-        displayAlert("The update settings feature will be implemented in beta")
+        if (greenInput.text != "" && orangeInput.text != ""){
+            if (isStringAnInt(string: greenInput.text!) && isStringAnInt(string: orangeInput.text!)){
+                if (Int(greenInput.text!)! < Int(orangeInput.text!)! ){
+                saveSettings(notifications: notifications.isOn, rememberLogin: rememberLogin.isOn, greenInput: Int(greenInput.text!)!, orangeInput: Int(orangeInput.text!)!)
+                    displayAlert("Your Settings have been updated")
+                } else {
+                displayAlert("The green amount must be smaller than the orange amount")
+                }
+            } else {
+                displayAlert("You must enter a valid number")
+            }
+        }
+        else {
+            displayAlert("You must enter a valid number")
+        }
     }
     
     func displayAlert (_ message: String){
@@ -46,5 +87,64 @@ class SettingViewController: UIViewController, UITextFieldDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    fileprivate func saveSettings(notifications: Bool, rememberLogin: Bool, greenInput: Int, orangeInput: Int) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // Create the entity we want to save
+        let entity =  NSEntityDescription.entity(forEntityName: "Settings", in: managedContext)
+        
+        let setting = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        // Set the attribute values
+        setting.setValue(greenInput, forKey: "greenSetting")
+        setting.setValue(orangeInput, forKey: "orangeSetting")
+        setting.setValue(notifications, forKey: "notifications")
+        setting.setValue(rememberLogin, forKey: "rememberLogin")
+        // Commit the changes.
+        do {
+            try managedContext.save()
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            print("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        // Add the new entity to our array of managed objects
+            settings.append(setting)
+        
+    }
+    func isStringAnInt(string: String) -> Bool {
+        return Int(string) != nil
+    }
+    
+    fileprivate func loadData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Settings")
+        
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        do {
+            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
+        } catch {
+            // what to do if an error occurs?
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        if let results = fetchedResults {
+            settings = results
+        } else {
+            print("Could not fetch")
+        }
     }
 }
